d4268befdde05a2b33765527702b39841cfbd449
FAI8h <sazaralam20074@gmail.com>
2026-09-03 09:25:12 +0530

    removed move constructor / move assignment

--- memory-managements/rule-of-0.cpp
+++ memory-managements/rule-of-0.cpp
@@ -3,19 +3,70 @@
 
 using namespace std;
 
-class Myvector {
+class MyVector {
 private:
     unique_ptr<int[]> data;
     int size;
     int capacity;
 public:
+    MyVector(int size = 0, int capacity = 2) 
+        : size(size), capacity(capacity){
+            data = make_unique<int[]>(capacity);
+        };
+
+    //* this is copy constructor
+    MyVector(const MyVector &other)
+        : size(other.size), capacity(other.capacity){
+            data = make_unique<int[]>(other.capacity);
+
+        for(int i = 0; i < other.size; i++){
+            data[i] = other.data[i];
+        }
+    }
+
+    //* this is copy assignment
+    MyVector &operator=(const MyVector &other) {
+        if(this == &other) return *this;
+
+        this->capacity = other.capacity;
+        this->size = other.size;
+        data = make_unique<int[]>(other.capacity);
+
+        for (int i = 0; i < other.size; i++){
+            data[i] = other.data[i];
+        }
+
+        return *this;
+    }
+    int& operator[](int index){
+        return data[index];
+    }
+
+    void push_back(int val){
+        if(size == capacity){
+            capacity = capacity * 2;
+            auto newArr = make_unique<int[]>(capacity);
+
+            for (int i = 0; i < size; i++){
+                newArr[i] = data[i];
+            }
+            data = move(newArr);
+        }
+        data[size] = val;
+        size++;
+    }
 
 };
 
 int main(){
-    Myvector a;
-    
-    Myvector b = a;
+    MyVector a;
+    a.push_back(1);
 
-    return 0;
+    MyVector b = a;
+
+    for (int i = 0; i < 5; i++){
+        cout << "data a : "<<a[i] << " addr a : "<< &a[i] << "data b : "<<b[i] << " addr b : "<< &b[i] <<endl;
+    }
+
+        return 0;
 }

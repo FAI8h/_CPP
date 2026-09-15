9042bc6d2af49627333a9827b720d910e5144ca6
FAI8h <sazaralam20074@gmail.com>
2026-09-15 11:04:21 +0530

    Weak Ptr : created a shared control Block

---
+++ memory-managements/weak_ptr.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct ControlBlock{
+    int strongCount;
+    int weakCount;
+};
+
+template <typename T>
+class SharedPtr{
+private:
+    T *ptr;
+    ControlBlock *ctrl;
+
+    // Friends
+    friend class weak_ptr<T>;
+
+    void release(){
+        if(this->ctrl == nullptr) return; //* this prevents the seg* fault when releasing on a null refCount
+
+        (this->ctrl->strongCount)--;
+        if(this->ctrl->strongCount <= 0){
+            delete this->ptr;
+        }
+        
+        if(this->ctrl->weakCount <= 0 && this->ctrl->strongCount <= 0){
+            delete this->ctrl;
+        }
+    }
+
+public:
+    explicit SharedPtr(T* p = nullptr){
+        this->ptr = p;
+        this->ctrl = new ControlBlock{1, 0};
+    }
+
+    ~SharedPtr() { release(); };
+
+    //* copy Constructor
+    SharedPtr(const SharedPtr& other){
+        this->ptr = other.ptr;
+        this->ctrl = other.ctrl;
+        (this->ctrl->strongCount)++;
+    }
+
+    //* copy Assignment
+    SharedPtr& operator=(const SharedPtr& other){
+        if(this == &other) return *this;
+
+        release();
+
+        this->ptr = other.ptr;
+        this->ctrl = other.ctrl;
+        (this->ctrl->strongCount)++;
+
+        return *this;
+    }
+
+    //* move Constructor
+    SharedPtr(SharedPtr&& other) noexcept {
+        this->ptr = other.ptr;
+        this->ctrl = other.ctrl;
+
+        other.ptr = nullptr;
+        other.ctrl = nullptr;
+    }
+
+    //* move Assignment
+    SharedPtr& operator=(SharedPtr&& other) noexcept {
+        if(this == &other) return *this;
+        release();
+
+        this->ptr = other.ptr;
+        this->ctrl = other.ctrl;
+
+        other.ptr = nullptr;
+        other.ctrl = nullptr;
+
+        return *this;
+    }
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    int use_count() const {
+        return this->ctrl->strongCount;
+    }
+};
+
+template <typename T>
+class WeakPtr{
+    private:
+        T *ptr;
+        ControlBlock* ctrl;
+
+    public:
+        WeakPtr(const SharedPtr<T> & sp){
+            this->ptr = sp.ptr;
+            this->ctrl = sp.ctrl;
+            this->ctrl->weakCount++;
+        };
+
+
+};
+
+int main(){
+
+    SharedPtr<int> a(new int(10));
+
+    cout << a.use_count() << endl;
+
+    return 0;
+}

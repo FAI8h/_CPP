89ec0e9b5dbdc80875ce601c92efbef0edb82400
FAI8h <sazaralam20074@gmail.com>
2026-09-16 11:47:57 +0530

    arena allocator : fixed  memory(used) offset bytes

--- memory-managements/arena-allocator.cpp
+++ memory-managements/arena-allocator.cpp
@@ -35,7 +35,7 @@ public:
         }
         T* addr = reinterpret_cast<T *>(buffer + localOffset);
 
-        offset += localOffset + bytesNeeded;
+        offset = localOffset + bytesNeeded;
         return addr;
     }
 };

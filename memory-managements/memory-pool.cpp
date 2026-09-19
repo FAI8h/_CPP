6ad0637e704dc83b3b5ed0effcdb326f086ad03f
FAI8h <sazaralam20074@gmail.com>
2026-09-19 11:32:56 +0530

    Add MemoryPool class implementation for dynamic memory management

---
+++ memory-managements/memory-pool.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using namespace std;
+
+template<typename T>
+class MemoryPool {
+private:
+    char *buffer;
+    size_t capacity;
+    void *freeListHead;
+
+public:
+    explicit MemoryPool(size_t numSlots){
+        size_t slotSize = max(sizeof(T), sizeof(void *));
+        this->buffer = static_cast<char *>(operator new(slotSize * numSlots));
+
+        int i = 0;
+        void **addr = nullptr;
+        for (; i < numSlots - 1; i++){
+            //addr
+            *addr = reinterpret_cast<void *>(buffer + i * slotSize);
+            *(addr) = reinterpret_cast<void *>(buffer + (i + 1) * slotSize);
+        }
+        addr = reinterpret_cast<T *>(buffer + i * slotSize);
+        *(addr) = nullptr;
+        
+
+    }
+};
+
+int main(){
+
+    return 0;
+}

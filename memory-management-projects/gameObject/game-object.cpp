26ac0eca319bb9122dfa8827c56fbe31ed407424
FAI8h <sazaralam20074@gmail.com>
2026-09-21 14:45:54 +0530

    introduced memory pool : now the game object will use custom memory pool & a custom backed deleter

--- memory-management-projects/gameObject/game-object.cpp
+++ memory-management-projects/gameObject/game-object.cpp
@@ -401,6 +401,63 @@ class WeakPtr{
         int weak_count_debug() const { return this->ctrl ? this->ctrl->weakCount : 1; };
 };
 
+//* Memory pool
+template<typename T>
+class MemoryPool {
+private:
+    char *buffer;
+    size_t capacity;
+    void *freeListHead;
+
+public:
+    explicit MemoryPool(size_t numSlots){
+        size_t slotSize = max(sizeof(T), sizeof(void *));
+
+        this->capacity = slotSize * numSlots;
+        this->buffer = static_cast<char *>(operator new(slotSize * numSlots));
+
+        int i = 0;
+        void **addr = nullptr;
+        for (; i < numSlots - 1; i++){
+            //addr
+            addr = reinterpret_cast<void **>(buffer + i * slotSize);
+            *(addr) = reinterpret_cast<void *>(buffer + (i + 1) * slotSize);
+        }
+        addr = reinterpret_cast<void **>(buffer + i * slotSize);
+        *(addr) = nullptr;
+
+        this->freeListHead = this->buffer;
+    }
+
+    ~MemoryPool(){
+        ::operator delete(buffer);
+    }
+
+    T* allocate(){
+        if(this->freeListHead == nullptr){
+            throw std::out_of_range("pool exhausted");
+        }
+
+        void *slot = this->freeListHead;
+
+        void * next = *reinterpret_cast<void **>(slot);
+
+        this->freeListHead = next;
+
+        return reinterpret_cast<T *>(slot);
+    }
+
+    void deallocate(T* ptr){
+        char *p = reinterpret_cast<char *>(ptr);
+        if (this->buffer > p || this->buffer + this->capacity <= p){
+            throw std::range_error("bad pointer access");
+        }
+
+        *reinterpret_cast<void **>(ptr) = this->freeListHead;
+        this->freeListHead = ptr;
+    }
+};
+
 //* --------------------------------game object---------------------------------------
 
 class GameObject{
